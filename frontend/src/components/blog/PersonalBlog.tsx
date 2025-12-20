944d162d3ff1139f6e15e13d813fc007425448ba
Peeroy <peeroy@localhost.localdomain>
2025-12-20 11:42:58 -0500

    Fixed formatting and preparing for deployment.

--- frontend/src/components/blog/PersonalBlog.tsx
+++ frontend/src/components/blog/PersonalBlog.tsx
@@ -9,22 +9,22 @@ const PersonalBlog: React.FC = () => (
     {/* August 20, 2025 Entry */}
     <h2>August 20, 2025</h2>
 
-    <p>Since this is the first entry, I'll introduce myself <i>informally</i> now that the requirement didn't follow you to this page.</p>
-    <p>I'm Edwin Gabriel Villanueva. I grew up in Port Saint Lucie and I have always wanted to work with computers. </p>
-    <p>As you may have been able to tell from the design of my website, I like Nintendo. I have since some of my first conscious memories.   </p>
-    <p>My ultimate personal goal is to eventually land a software engineering position for them in Redmond, Washington. </p>
-
-    <p>Here's some context behind that decision. </p>
+    <p> Since this is the first entry, I'll introduce myself <i>informally</i> now that the requirement didn't follow you to this page.</p>
+    <p>I'm Edwin Gabriel Villanueva. I grew up in Port Saint Lucie and I have always wanted to work with computers. As you may have been able to tell from the design of my website, I like Nintendo. I have since some of my first conscious memories. My ultimate personal goal is to eventually land a software engineering position for them in Redmond, Washington. Here's some context behind that decision. </p>
     
     <p>Since the day I learned how to search on Google, I've always kept up with news and updates from Nintendo, including Nintendo Direct streams. But for some reason, I always stayed away from the hope that, when I get older, I'd want to work for them, as it seemed entirely impossible (I was also only eleven, I didn't know what made something like this possible). </p>
-    <p>It stayed that way until Nintendo streamed their April 1st, 2015 direct (10 years - wow). I watched it and my curiosity could no longer be contained, so I dug around and found their careers page (remember, I was 11 at the time)</p>
-    <p>Since finding this page, it's been the only thing on my mind. Even now.</p>
+    <p>It stayed that way until Nintendo streamed their April 1st, 2015 direct (10 years - wow). I watched it and my curiosity could no longer be contained, so I dug around and found their careers page (remember, I was 11 at the time). Since finding this page, it's been the only thing on my mind. Even now.</p>
 
     <p></p>
 
     <p>Week 1 of my last year at UCF just started. It feels weird that I won't be going here after next spring.</p>
 
-    <p></p>
+    <p></p><p/><br/>
+
+    {/* December 20, 2025 Entry */}
+    <h2>December 20, 2025</h2>
+    <p>This semester has been so eventful. It ended a short while ago, but yesterday, I also just completed my last day at my internship with CPT for this semester. I feel like it's only been a little while since New Years going into 2024 even. Like life is moving so fast, but it's taking its time and showing me so much at the same time. </p>
+  
   </div>
 );
 
